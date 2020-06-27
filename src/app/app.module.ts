f6badd24d95ff4aa0d14aa7471e4d7ee045a9241
Luis Guevara <luisfelipe.guevaraa@gmail.com>
2020-06-27 08:13:02 -0500

    Input, Output y ViewChild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 // Rutas
 import { APP_ROUTES } from './app.routes';
 
 
 // Modulos
 import { PagesModule } from './pages/pages.module';
+
+//Temporal
+import { FormsModule } from '@angular/forms';
 
 // Componentes
 import { AppComponent } from './app.component';
@@ -22,7 +26,8 @@ import { RegisterComponent } from './login/register.component';
   imports: [
     BrowserModule,
     APP_ROUTES,
-    PagesModule
+    PagesModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
